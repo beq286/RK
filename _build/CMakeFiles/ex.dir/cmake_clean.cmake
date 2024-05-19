5faea003d4e35251b70bde78836d1e29d9788117
beq286 <beqisma@yandex.ru>
2024-05-19 20:50:44 +0300

    build CMakeLists Example 1 & Example 2

---
+++ _build/CMakeFiles/ex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex.dir/ConceptualExample01.cpp.o"
+  "CMakeFiles/ex.dir/ConceptualExample01.cpp.o.d"
+  "libex.a"
+  "libex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/ex2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2.dir/ConceptualExample02.cpp.o"
+  "CMakeFiles/ex2.dir/ConceptualExample02.cpp.o.d"
+  "libex2.a"
+  "libex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
