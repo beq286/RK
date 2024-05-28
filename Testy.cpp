da95bd9dcc417e7e2846a6ae7340613e27980667
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-28 13:34:52 +0300

    Update Testy.cpp

--- Testy.cpp
+++ Testy.cpp
@@ -34,23 +34,6 @@ TEST(ConceptualExample02Test, FactoryMethodTest) {
     // Возвращаем stdout в исходное состояние
     std::cout.rdbuf(oldOutput);
 }
-
-TEST(ConceptualExample02Test, FactoryMethodTest) {
-    // Arrange
-    std::stringstream output;
-    std::streambuf* oldOutput = std::cout.rdbuf(output.rdbuf());  // Перенаправляем stdout в строку для проверки вывода
-
-    // Act
-    test_conceptual_example_02();  
-
-    // Assert
-    std::string expectedOutput = "Example launched with ConcreteFactory1:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct1\"\n\nExample launched with ConcreteFactory2:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct2\"\n";
-    ASSERT_EQ(output.str(), expectedOutput);  // Проверяем, что вывод соответствует ожидаемому
-
-    // Возвращаем stdout в исходное состояние
-    std::cout.rdbuf(oldOutput);
-}
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
