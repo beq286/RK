205d04eeec5a4c48601ed6232a7219dd62bb88d2
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-28 12:24:42 +0300

    Update Testy.cpp

--- Testy.cpp
+++ Testy.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
-#include "ConceptualExample01.cpp"  
+#include "ConceptualExample01.cpp"
+#include "ConceptualExample02.cpp"  
 
 TEST(ConceptualExample01Test, FactoryMethodTest) {
     // Arrange
@@ -11,6 +12,22 @@ TEST(ConceptualExample01Test, FactoryMethodTest) {
 
     // Assert
     std::string expectedOutput = "{ Product1 }\n{ Product2 }\n";
+    ASSERT_EQ(output.str(), expectedOutput);  // Проверяем, что вывод соответствует ожидаемому
+
+    // Возвращаем stdout в исходное состояние
+    std::cout.rdbuf(oldOutput);
+}
+
+TEST(ConceptualExample02Test, FactoryMethodTest) {
+    // Arrange
+    std::stringstream output;
+    std::streambuf* oldOutput = std::cout.rdbuf(output.rdbuf());  // Перенаправляем stdout в строку для проверки вывода
+
+    // Act
+    test_conceptual_example_02();  
+
+    // Assert
+    std::string expectedOutput = "Example launched with ConcreteFactory1:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct1\"\n\nExample launched with ConcreteFactory2:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct2\"\n";
     ASSERT_EQ(output.str(), expectedOutput);  // Проверяем, что вывод соответствует ожидаемому
 
     // Возвращаем stdout в исходное состояние
