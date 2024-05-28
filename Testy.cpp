b0e2e5a41eb0cb8e988952fd4e932242bdda695f
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-28 13:33:18 +0300

    Update Testy.cpp

--- Testy.cpp
+++ Testy.cpp
@@ -35,19 +35,20 @@ TEST(ConceptualExample02Test, FactoryMethodTest) {
     std::cout.rdbuf(oldOutput);
 }
 
-TEST(RealWorldExampleTest, TestLEDTVFactory) {
-    testing::internal::CaptureStdout();
+TEST(ConceptualExample02Test, FactoryMethodTest) {
+    // Arrange
+    std::stringstream output;
+    std::streambuf* oldOutput = std::cout.rdbuf(output.rdbuf());  // Перенаправляем stdout в строку для проверки вывода
 
-    RealWorldFactoryMethod::LEDTVFactory factory;
-    RealWorldFactoryMethod::clientCode01(std::make_shared<RealWorldFactoryMethod::LEDTVFactory>());
+    // Act
+    test_conceptual_example_02();  
 
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "Manufacturing LED TV\nShipping charge: 1000 Euro\n");
+    // Assert
+    std::string expectedOutput = "Example launched with ConcreteFactory1:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct1\"\n\nExample launched with ConcreteFactory2:\nClient (doesn't know the factory's concrete class type)\nFactoryBase: This factory's code has just created a \"Result of the ConcreteProduct2\"\n";
+    ASSERT_EQ(output.str(), expectedOutput);  // Проверяем, что вывод соответствует ожидаемому
 
-    testing::internal::CaptureStdout();
-    RealWorldFactoryMethod::clientCode02(std::make_shared<RealWorldFactoryMethod::LEDTVFactory>());
-    output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "My new television receiver costs me 2500\nSwitched on my LED TV\nMy new TV is a LED TV device.\n");
+    // Возвращаем stdout в исходное состояние
+    std::cout.rdbuf(oldOutput);
 }
 
 int main(int argc, char **argv) {
