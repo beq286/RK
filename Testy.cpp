0a4cad5efbd259bb622e786b4ea457658b24a14b
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-28 11:57:42 +0300

    Update Testy.cpp

--- Testy.cpp
+++ Testy.cpp
@@ -1,41 +1,24 @@
 #include <gtest/gtest.h>
 #include "ConceptualExample01.cpp"
 #include "ConceptualExample02.cpp"
+#include "RealWorldFactoryMethod.cpp"
+#include "Points.cpp"
+#include <iostream>
 
-TEST(ConcreteCreator1, example) {
-    Creator* creator = new ConcreteCreator1();
-    Product* product = creator->factoryMethod();
 
-    // Проверяем, что продукт является экземпляром ConcreteProduct1
-    ASSERT_TRUE(dynamic_cast<ConcreteProduct1*>(product) != nullptr);
-    delete product;
-    delete creator;
+
+TEST(BuilderTest, ConceptualExample01) {
+    ASSERT_NO_THROW(test_conceptual_example_01());
 }
 
-TEST(ConcreteCreator2, example) {
-    Creator* creator = new ConcreteCreator2();
-    Product* product = creator->factoryMethod();
-
-    // Проверяем, что продукт является экземпляром ConcreteProduct2
-    ASSERT_TRUE(dynamic_cast<ConcreteProduct2*>(product) != nullptr);
-
-    delete product;
-    delete creator;
+TEST(BuilderTest, ConceptualExample02) {
+    ASSERT_NO_THROW(test_conceptual_example_02());
 }
 
-TEST(ProductFunctionality,example) {
-    Creator* creator1 = new ConcreteCreator1();
-    Product* product1 = creator1->factoryMethod();
+TEST(BuilderTest, RealWorldFactoryMethod) {
+    ASSERT_NO_THROW(test_layout_manager_example());
+}
 
-    Creator* creator2 = new ConcreteCreator2();
-    Product* product2 = creator2->factoryMethod();
-
-    // Проверяем функциональность продуктов
-    ASSERT_EQ(product1->operation(), "Result of the ConcreteProduct1");
-    ASSERT_EQ(product2->operation(), "Result of the ConcreteProduct2");
-
-    delete product1;
-    delete creator1;
-    delete product2;
-    delete creator2;
+TEST(BuilderTest, Points) {
+    ASSERT_NO_THROW(test_person_builder_example());
 }
