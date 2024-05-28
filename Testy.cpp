dba5ec9ae384ecf621b831c213a7b9b50797d234
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-28 12:19:33 +0300

    Update Testy.cpp

--- Testy.cpp
+++ Testy.cpp
@@ -1,24 +1,23 @@
 #include <gtest/gtest.h>
-#include "ConceptualExample01.cpp"
-#include "ConceptualExample02.cpp"
-#include "RealWorldFactoryMethod.cpp"
-#include "Points.cpp"
-#include <iostream>
+#include "ConceptualExample01.cpp"  
 
+TEST(ConceptualExample01Test, FactoryMethodTest) {
+    // Arrange
+    std::stringstream output;
+    std::streambuf* oldOutput = std::cout.rdbuf(output.rdbuf());  // Перенаправляем stdout в строку для проверки вывода
 
+    // Act
+    test_conceptual_example_01();  
 
-TEST(BuilderTest, ConceptualExample01) {
-    ASSERT_NO_THROW(test_conceptual_example_01());
+    // Assert
+    std::string expectedOutput = "{ Product1 }\n{ Product2 }\n";
+    ASSERT_EQ(output.str(), expectedOutput);  // Проверяем, что вывод соответствует ожидаемому
+
+    // Возвращаем stdout в исходное состояние
+    std::cout.rdbuf(oldOutput);
 }
 
-TEST(BuilderTest, ConceptualExample02) {
-    ASSERT_NO_THROW(test_conceptual_example_02());
-}
-
-TEST(BuilderTest, RealWorldFactoryMethod) {
-    ASSERT_NO_THROW(test_real_world_factory_method());
-}
-
-TEST(BuilderTest, Points) {
-    ASSERT_NO_THROW(test_points());
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
